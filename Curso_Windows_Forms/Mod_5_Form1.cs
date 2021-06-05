c53c1a0b9bf4f3f3393dc62233e5c16f41322a1d
Thalles Bruno <thallesbgns@gmail.com>
2021-06-04 23:23:31 -0300

    Trabalhando com arquivos 1

---
+++ Curso_Windows_Forms/Mod_5_Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curso_Windows_Forms
+{
+    public partial class Mod_5_Form1 : Form
+    {
+        public Mod_5_Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Primeira forma
+            //MessageBox.Show(System.Environment.CurrentDirectory);
+
+            //Segunda forma
+            MessageBox.Show(System.AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String nomeArq = "Arquivo.txt";
+            Stream arquivo = File.Create(nomeArq);
+        }
+    }
+}
