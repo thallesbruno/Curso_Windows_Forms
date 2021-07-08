5c4c974a36a5b9db4a7920d29c7a56766c51ec6a
Thalles Bruno <thallesbgns@gmail.com>
2021-07-08 19:33:39 -0300

    Orientacao a Objetos 1

---
+++ ProjetoModuloSete/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModuloSete
+{
+    public class Carro
+    {
+        String cor;
+        String modelo;
+        Double velocidadeMaxima;
+        int qtdRodas;
+        int qtdPortas;
+    }
+}
