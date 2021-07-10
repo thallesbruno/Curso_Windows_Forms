69944b11cbfac9394a1dbbb9d4aba3da8c6a06c5
Thalles Bruno <thallesbgns@gmail.com>
2021-07-09 23:20:33 -0300

    Encapsulamento

--- ProjetoModuloSete/Carro.cs
+++ ProjetoModuloSete/Carro.cs
@@ -4,13 +4,63 @@ namespace ProjetoModuloSete
 {
     public class Carro
     {
-        public String cor;
-        public String modelo;
-        public Double velocidadeMaxima;
-        public int qtdRodas;
-        public int qtdPortas;
+        private String cor;
+        private String modelo;
+        private Double velocidadeMaxima;
+        private int qtdRodas;
+        private int qtdPortas;
 
-       public string Ligar()
+        public void setCor(string cor)
+        {
+            this.cor = cor;
+        }
+
+        public string getCor()
+        {
+            return this.cor;
+        }
+
+        public void setModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+
+        public string getModelo()
+        {
+            return this.modelo;
+        }
+
+        public void setVelocidadeMaxima(double velocidadeMaxima)
+        {
+            this.velocidadeMaxima = velocidadeMaxima;
+        }
+
+        public double getVelocidadeMaxima()
+        {
+            return this.velocidadeMaxima;
+        }
+
+        public void setQtdRodas(int qtdRodas)
+        {
+            this.qtdRodas = qtdRodas;
+        }
+
+        public int getQtdRodas()
+        {
+            return this.qtdRodas;
+        }
+
+         public void setQtdPortas(int qtdPortas)
+        {
+            this.qtdPortas = qtdPortas;
+        }
+
+        public int getQtdPortas()
+        {
+            return this.qtdPortas;
+        }
+
+        public string Ligar()
         {
             return "Carro ligado!";
         }
--- ProjetoModuloSete/Form1.Designer.cs
+++ ProjetoModuloSete/Form1.Designer.cs
@@ -68,7 +68,7 @@ namespace ProjetoModuloSete
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 264);
+            this.ClientSize = new System.Drawing.Size(332, 264);
             this.Controls.Add(this.btnLigar);
             this.Controls.Add(this.btnAcelerar);
             this.Controls.Add(this.btnCriar);
