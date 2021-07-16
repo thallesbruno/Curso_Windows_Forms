99409de74cdc0d0bbd6cc92dd4074945ecb0d0a4
Thalles Bruno <thallesbgns@gmail.com>
2021-07-16 08:10:39 -0300

    Antes de Propriedades Automáticas

--- ProjetoModuloSete/Carro.cs
+++ ProjetoModuloSete/Carro.cs
@@ -13,41 +13,51 @@ namespace ProjetoModuloSete
             setQtdRodas(qtdRodas);*/
         }
 
-        private String cor;
-        private String modelo;
-        private Double velocidadeMaxima;
-        private int qtdRodas;
-        private int qtdPortas;
+        //private String cor;
+        public String cor { get; set; }
+        //private String modelo;
+        public String modelo { get; set; }
+        //private Double velocidadeMaxima;
+        public Double VelocidadeMaxima { get; set; }
+        //private int qtdRodas;
+        public int qtdRodas { get; set; }
+        //private int qtdPortas;
+        public int qtdPortas { get; set; }
 
-        public string Cor
+        //Cor
+        /*public string Cor
         {
             get{ return this.cor; }
             set { this.cor = value; }
         }
 
+        //Modelo
         public string Modelo
         {
             get{ return this.modelo; }
             set { this.modelo = value; }
         }
 
+        //VelocidadeMaxima
         public double VelocidadeMaxima
         {
             get { return this.velocidadeMaxima; }
             set { this.velocidadeMaxima = value; }
         }
 
+        //QtdRodas
         public int QtdRodas
         {
             get { return this.qtdRodas; }
             set { this.qtdRodas = value; }
         }
 
+        //QtdPortas
         public int QtdPortas
         {
             get { return this.qtdPortas; }
             set { this.qtdPortas = value; }
-        }
+        }*/
 
         public string Ligar()
         {
