93cb8fe8df1809a360e4c1739d079c8e21516f13
Thalles Bruno <thallesbgns@gmail.com>
2021-07-16 22:00:20 -0300

    Herança I

--- ProjetoModuloSete/Carro.cs
+++ ProjetoModuloSete/Carro.cs
@@ -2,27 +2,8 @@
 
 namespace ProjetoModuloSete
 {
-    public class Carro
+    public class Carro : Veiculo
     {
-        public String cor { get; set; }
-        public String modelo { get; set; }
-        public Double velocidadeMaxima { get; set; }
-        public int qtdRodas { get; set; }
-        public int qtdPortas { get; set; }
-        
-        public string Ligar()
-        {
-            return "Carro ligado!";
-        }
-        public string Ligar(int velocidade)
-        {
-            return "Carro ligado! Velocidade inicial: " + velocidade;
-        }
-        public string Ligar(double velocidade)
-        {
-            return "Carro ligado! Velocidade inicial: " + velocidade;
-        }
-
         public string Acelerar()
         {
             return "Carro acelerou!";
---
+++ ProjetoModuloSete/Veiculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModuloSete
+{
+    public class Veiculo
+    {
+        public String cor { get; set; }
+        public String modelo { get; set; }
+        public Double velocidadeMaxima { get; set; }
+        public int qtdRodas { get; set; }
+        public int qtdPortas { get; set; }
+
+        public string Ligar()
+        {
+            return "Veículo ligado!";
+        }
+        public string Ligar(int velocidade)
+        {
+            return "Veículo ligado! Velocidade inicial: " + velocidade;
+        }
+        public string Ligar(double velocidade)
+        {
+            return "Veículo ligado! Velocidade inicial: " + velocidade;
+        }
+
+        public string Acelerar()
+        {
+            return "Veículo acelerou!";
+        }
+    }
+}
